4db387dc8db801a3abf1aa73c2487c8c0b273f8e
京子 <ajw29@pdx.edu>
2019-11-01 22:08:50 -0700

    iframe edition
    Removed bootstrap carousel and put it into an iframe. Edited Css

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+var sidebar = true;
+
 $(document).ready(function(){
   $("#toggleside").click(function(){
     $("#sidebar").toggleClass("hiding");
     $("#main").toggleClass("nomarg");
+    if(sidebar){
+    	$("#toggleside").val("»");
+    	sidebar = false;
+    }else{
+    	$("#toggleside").val("«");
+    	sidebar = true;
+    }
   });
 });
 
